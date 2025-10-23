055e93da69cdbde7ee368bb233dd1071b0bbf380
MICKAEL LUAN TRAPP <mickael_trapp@estudante.sesisenai.org.br>
2025-10-23 11:07:56 -0300

    exercicios2 1 a 13
    git push
    clear

---
+++ exercicios 2/exercicio3.py
@@ -0,0 +1,10 @@
+print("M - Masculino | F - Feminino | O - Outros")
+sexo = input("Digite a letra correspondente ao seu sexso: ")
+
+if sexo == "M" or sexo == "m":
+    print("Você é do sexo Masculino")
+elif sexo == "F" or sexo == "f":
+    print("Você é do sexo Feminino")
+else:
+    print("Você preferio não informar")
+
